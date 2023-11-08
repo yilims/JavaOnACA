814dc6a1fc296c9b638162118526168e2c7a51eb
Zhiyong Li <iamlizhiyong@outlook.com>
2023-11-08 19:47:56 +0800

    Update appup-17.sh

--- appup-17.sh
+++ appup-17.sh
@@ -14,33 +14,27 @@ repourl=https://github.com/showpune/spring-petclinic
 repoappnoreg=spring-repo-17-no-registry
 repourl=https://github.com/showpune/spring-petclinic
 
-#az containerapp up -n $sourceappall -g $rg  --environment $enviroment --source $sourcepathall --ingress external --target-port 8080 --subscription $subscription --location $location 
-# echo 'Deploy with Jar'
-# az containerapp delete -n $jarapp -g $rg --subscription $subscription
-# az containerapp up -n $jarapp -g $rg  --environment $enviroment --artifact $jarpath --ingress external --target-port 8080 --subscription $subscription --location $location 
+az containerapp up -n $sourceappall -g $rg  --environment $enviroment --source $sourcepathall --ingress external --target-port 8080 --subscription $subscription --location $location 
+echo 'Deploy with Jar'
+az containerapp delete -n $jarapp -g $rg --subscription $subscription --yes
+az containerapp up -n $jarapp -g $rg  --environment $enviroment --artifact $jarpath --ingress external --target-port 8080 --subscription $subscription --location $location 
 
-# echo 'Deploy with source code'
-# az containerapp delete -n $sourceapp -g $rg --subscription $subscription
-# az containerapp up -n $sourceapp -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location 
+echo 'Deploy with source code'
+az containerapp delete -n $sourceapp -g $rg --subscription $subscription --yes
+az containerapp up -n $sourceapp -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location 
 
-# echo 'Deploy with source code and all'
-# az containerapp delete -n $sourceappall -g $rg --subscription $subscription
-# az containerapp up -n $sourceappall -g $rg  --environment $enviroment --source $sourcepathall --ingress external --target-port 8080 --subscription $subscription --location $location
+echo 'Deploy with source code and all'
+az containerapp delete -n $sourceappall -g $rg --subscription $subscription --yes
+az containerapp up -n $sourceappall -g $rg  --environment $enviroment --source $sourcepathall --ingress external --target-port 8080 --subscription $subscription --location $location
 
+echo 'Deploy with source code with local builder'
+az containerapp delete -n "${sourceapp}-local" -g $rg --subscription $subscription --yes
+az containerapp up -n "${sourceapp}-local" -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
 
-#echo 'Deploy with source code with local builder'
-#az containerapp delete -n "${sourceapp}-repo" -g $rg --subscription $subscription
-#az containerapp up -n "${sourceapp}-repo" -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
-
-
-# echo 'Deploy with source code with repo'
-# #az containerapp delete -n "${sourceapp}-repo" -g $rg --subscription $subscription
-# az containerapp up -n "${sourceapp}-repo" -g $rg  --environment $enviroment --repo "https://github.com/showpune/spring-petclinic" --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
-
-# echo 'Deploy with source code with repo'
-# #az containerapp delete -n "${repoapp}" -g $rg --subscription $subscription
-# az containerapp up -n "${repoapp}" -g $rg  --environment $enviroment --repo $repourl --ingress external --target-port 8080 --subscription $subscription --location $location 
+echo 'Deploy with source code with repo'
+az containerapp delete -n "${repoapp}" -g $rg --subscription $subscription --yes
+az containerapp up -n "${repoapp}" -g $rg  --environment $enviroment --repo $repourl --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
 
 echo 'Deploy with source code with repo no registery'
-#az containerapp delete -n "${repoappnoreg}"-g $rg --subscription $subscription
+az containerapp delete -n "${repoappnoreg}" -g $rg --subscription $subscription --yes
 az containerapp up -n "${repoappnoreg}" -g $rg  --environment $enviroment --repo $repourl --ingress external --target-port 8080 --subscription $subscription --location $location 
