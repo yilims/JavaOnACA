55888f1af93b0ed0b154f424dfd198eda6041ff1
Yi Li <109205537+yilims@users.noreply.github.com>
2023-11-09 16:05:18 +0800

    refine env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
-enviroment=xxx-stage-env
-rg=xxx-stage-group
+enviroment=xxx-bugbash-env
+rg=xxx-bugbash-rg
 subscription=d0822b01-62ea-4eb9-885b-95c60e4250b4
 registryserver=xxx.azurecr.io
 registryuser=xxx
-registerpass=
+registerpass=xxx
 location=northcentralusstage
 repourl=https://github.com/xxx/spring-petclinic
