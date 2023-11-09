675d54e5ae7bba7d15389dfcc3a5ceb62395777e
yilims <brightmonkeyli@163.com>
2023-11-09 17:25:27 +0800

    refine test script

--- appup-17.sh
+++ appup-17.sh
@@ -10,7 +10,6 @@ sourceappall=spring-source-all-17
 repoapp=spring-repo-17
 repoappnoreg=spring-repo-17-no-registry
 
-az containerapp up -n $sourceappall -g $rg  --environment $enviroment --source $sourcepathall --ingress external --target-port 8080 --subscription $subscription --location $location 
 echo 'Deploy with Jar'
 az containerapp delete -n $jarapp -g $rg --subscription $subscription --yes
 az containerapp up -n $jarapp -g $rg  --environment $enviroment --artifact $jarpath --ingress external --target-port 8080 --subscription $subscription --location $location 
@@ -19,15 +18,15 @@ echo 'Deploy with source code'
 az containerapp delete -n $sourceapp -g $rg --subscription $subscription --yes
 az containerapp up -n $sourceapp -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location 
 
-echo 'Deploy with source code and all'
+echo 'Deploy with source code with mvn and github related files'
 az containerapp delete -n $sourceappall -g $rg --subscription $subscription --yes
 az containerapp up -n $sourceappall -g $rg  --environment $enviroment --source $sourcepathall --ingress external --target-port 8080 --subscription $subscription --location $location
 
-echo 'Deploy with source code with local builder'
+echo 'Deploy with source code with local builder, need to start Docker Engine first'
 az containerapp delete -n "${sourceapp}-local" -g $rg --subscription $subscription --yes
 az containerapp up -n "${sourceapp}-local" -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
 
-echo 'Deploy with source code with repo'
+echo 'Deploy with source code from git repo'
 az containerapp delete -n "${repoapp}" -g $rg --subscription $subscription --yes
 az containerapp up -n "${repoapp}" -g $rg  --environment $enviroment --repo $repourl --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
 
