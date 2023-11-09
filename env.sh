f9f31acecef67ed3927f1ab63baf257b2e79ff93
yilims <brightmonkeyli@163.com>
2023-11-09 19:19:00 +0800

    refine env

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
-enviroment=xxx-bugbash-env
-rg=xxx-bugbash-rg
 subscription=d0822b01-62ea-4eb9-885b-95c60e4250b4
+location=northcentralusstage
+rg=xxx-bugbash-rg
+enviroment=xxx-bugbash-env
 registryserver=xxx.azurecr.io
 registryuser=xxx
 registerpass=xxx
-location=northcentralusstage
 repourl=https://github.com/xxx/spring-petclinic
