8a19a61da62bcc5bb42bbfb169bad46d17dd744e
yilims <brightmonkeyli@163.com>
2023-11-09 22:03:50 +0800

    refine test script

--- appup-17.sh
+++ appup-17.sh
@@ -10,15 +10,11 @@ sourceappall=spring-source-all-17
 repoapp=spring-repo-17-with-registry
 repoappnoreg=spring-repo-17-no-registry
 
-az containerapp up -n $sourceappall -g $rg  --environment $enviroment --source $sourcepathall --ingress external --target-port 8080 --subscription $subscription --location $location 
 echo 'Deploy Jar'
 az containerapp up -n $jarapp -g $rg  --environment $enviroment --artifact $jarpath --ingress external --target-port 8080 --subscription $subscription --location $location 
 
 echo 'Deploy source code'
 az containerapp up -n $sourceapp -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location 
-
-echo 'Deploy source code with mvn and github related files'
-az containerapp up -n $sourceappall -g $rg  --environment $enviroment --source $sourcepathall --ingress external --target-port 8080 --subscription $subscription --location $location
 
 echo 'Deploy source code with local builder, requires docker engine started'
 az containerapp up -n "${sourceapp}-local" -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
