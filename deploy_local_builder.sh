412a5b48a2555f952a87982a86f91d88bc8475f2
yilims <brightmonkeyli@163.com>
2023-11-10 09:13:44 +0800

    refine test script

--- deploy_local_builder.sh
+++ deploy_local_builder.sh
@@ -8,4 +8,4 @@ sourceapp=spring-source-17
 repoapp=spring-repo-17-with-registry
 repoappnoreg=spring-repo-17-no-registry
 echo 'Deploy source code with local builder, please start docker engine first.'
-az containerapp up -n "${sourceapp}-local" -g $rg  --environment $enviroment --artifact $jarpath --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
+az containerapp up -n "${sourceapp}-local" -g $rg  --environment $enviroment --source $sourcepath --ingress external --target-port 8080 --subscription $subscription --location $location --registry-server $registryserver --registry-user $registryuser --registry-pass $registerpass
